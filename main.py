cb38c1cc4435a54bd94a8d51cb228fda830e7cad
Jovan Arsic <joca.arsic@hotmail.com>
2025-06-21 20:32:07 +0200

    Resolved conflict between master and add-cart-functionality

--- main.py
+++ main.py
@@ -5,15 +5,9 @@ def main():
     manager = ProductManager()
 
     # Dodavanje proizvoda
-    manager.add_product(Product("Laptop", 80000, 5))
-    manager.add_product(Product("Mobilni telefon", 50000, 10))
+    manager.add_product(Product("Tablet", 80000, 5))
+    manager.add_product(Product("Smartphone", 50000, 10))
     manager.add_product(Product("Smartwatch", 40000, 12))
-
-    # Prikaz proizvoda
-    manager.display_all_products()
-
-    # Prikaz ukupne vrednosti
-    print(f"Ukupna vrednost inventara: {manager.total_value()} RSD.")
 
 if __name__ == "__main__":
     main()
