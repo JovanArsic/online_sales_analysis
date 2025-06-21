86d9b9ea82bf05adb70f31800d9e33e80917cafa
Jovan Arsic <joca.arsic@hotmail.com>
2025-06-21 20:24:43 +0200

    Added Cart class and integrated cart functionality in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 def main():
     manager = ProductManager()
@@ -14,6 +15,16 @@ def main():
 
     # Prikaz ukupne vrednosti
     print(f"Ukupna vrednost inventara: {manager.total_value()} RSD.")
+
+
+    cart = Cart()
+    cart.add_to_cart(manager.products[0], 2)
+    cart.add_to_cart(manager.products[1], 1)
+    cart.add_to_cart(manager.products[2], 1)
+
+    print("Sadržaj korpe:")
+    cart.display_cart()
+    print(f"Ukupna cena za naplatu: {cart.total_price()} RSD.")
 
 if __name__ == "__main__":
     main()
